c8813741b34b65f3c4e5d082b3c25ed0561805a6
Sandeep Naidu <sandeep.n@quantiphi.com>
2019-11-26 17:30:53 +0530

    completed feedback details components

--- src/pages/admin/feedbacks/feedbacks.page.js
+++ src/pages/admin/feedbacks/feedbacks.page.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+// components
+import AppButton from '../../../components/AppButton/AppButton.Component';
+// firebase helper
 import { getFeedbacksByFaculty } from '../../../firebase/feedback.firebase';
 
 import './feedback.styles.scss';
@@ -11,8 +13,24 @@ class FeedbacksPage extends React.Component {
 
     this.state = {
       faculty: '',
-      feedbacks: []
+      feedbacks: [],
+      feedbackDetails: {},
+      showDetails: false,
     }
+  }
+
+  handleOnClickBack = () => {
+    this.setState({
+      showDetails: false,
+      feedbackDetails: {}
+    });
+  }
+
+  handleOnClickViewDetails = (details) => {
+    this.setState({
+      showDetails: true,
+      feedbackDetails: details,
+    })
   }
 
   async componentDidMount() {
@@ -31,27 +49,114 @@ class FeedbacksPage extends React.Component {
   }
 
   render() {
-    const { faculty, feedbacks } = this.state;
+    const {
+      faculty,
+      feedbacks,
+      showDetails,
+      feedbackDetails,
+    } = this.state;
     console.log('feedbacks', feedbacks);
+    console.log('feedbackDetails', feedbackDetails);
 
     return (
       <div className="feedbacks-page page-container">
         <div className="page-title">
-          <Link to="/admin">Back</Link>
+          {!showDetails ? (
+            <Link to="/admin" className="back">Back</Link>
+          ) : (
+            <div className="back" onClick={this.handleOnClickBack}>Back</div>
+          )}
           Fedbacks of {faculty}
         </div>
 
-        {
+        { !showDetails ? (
           feedbacks.map((feedback, index) => {
             return (
-              <div key={`${feedback.faculty}-${index}`}>
-                <div className="name">{ feedback.faculty }</div>
-                <div className="department">{ feedback.department }</div>
-                <div className="name">{ feedback.overallRating }</div>
+              <div key={`${feedback.faculty}-${index}`} className="feedback">
+                <div className="name">
+                  <p>Student ID</p>
+                  { feedback.user.studentId }
+                </div>
+                <div className="ratings">
+                  <p>Overall Rating</p>
+                  { feedback.overallRating }
+                </div>
+
+                <div className="details">
+                  <AppButton
+                    type="button"
+                    handleOnClick={() => this.handleOnClickViewDetails(feedback)}
+                  >
+                    View Details
+                  </AppButton>
+                </div>
               </div>
             )
           })
-        }
+        ) : (
+          <div className="feedback-details">
+            <p>
+              Student ID:
+              <span>{feedbackDetails.user.studentId}</span>
+            </p>
+            <p>
+              Department:
+              <span>{feedbackDetails.department}</span>
+            </p>
+            <p>
+              Subject:
+              <span>{feedbackDetails.subject}</span>
+            </p>
+            <p>
+              Comment:
+              <span>{feedbackDetails.comment}</span>
+            </p>
+            <p>
+              Passion and enthusiasm to teach:
+              <span>{feedbackDetails.passion}</span>
+            </p>
+            <p>
+              Subject Knowledge:
+              <span>{feedbackDetails.subjectKnowledge}</span>
+            </p>
+            <p>
+              Clarity and emphasis on concept:
+              <span>{feedbackDetails.clarityAndEmphasis}</span>
+            </p>
+            <p>
+              Motivational and inspiring students:
+              <span>{feedbackDetails.motivational}</span>
+            </p>
+            <p>
+              Creating interest in subject:
+              <span>{feedbackDetails.creatingIntrest}</span>
+            </p>
+            <p>
+              Quality illustrative examples and applications:
+              <span>{feedbackDetails.qualityOfIllustrative}</span>
+            </p>
+            <p>
+              Punctuality and uniform coverage of syllabus:
+              <span>{feedbackDetails.punctuality}</span>
+            </p>
+            <p>
+              Discipline and control in the class:
+              <span>{feedbackDetails.disipline}</span>
+            </p>
+            <p>
+              Promoting student thinking:
+              <span>{feedbackDetails.promotingStudent}</span>
+            </p>
+            <p>
+              Encouraging and student interaction:
+              <span>{feedbackDetails.encouraging}</span>
+            </p>
+            <p>
+              Overall Rating:
+              <span>{feedbackDetails.overallRating}</span>
+            </p>
+          </div>
+        )}
       </div>
     )
   }
